25889bef0cf4399d8339dd60bfd620114f251286
nmegrant <nme.grant@gmail.com>
2020-06-29 17:10:52 +0200

    Add calculations for tomorrow, life/set up dbcreate

--- routers/sentiment.js
+++ routers/sentiment.js
@@ -1,4 +1,4 @@
-const { Router } = require("express");
+const { Router, request } = require("express");
 const User = require("../models/").user;
 const Sentiment = require("../models/").sentiment;
 const SentimentTool = require("sentiment");
@@ -7,44 +7,57 @@ const sentiment = new SentimentTool();
 const router = new Router();
 
 router.post("/", async (req, res, next) => {
-  const { today, tomorrow, life } = req.body;
+  try {
+    const { today, tomorrow, life } = req.body;
 
-  console.log("today", today);
+    // console.log("today", today);
 
-  console.log("tomorrow", tomorrow);
+    // console.log("tomorrow", tomorrow);
 
-  console.log("life", life);
+    // console.log("life", life);
 
-  //res.send({ today, tomorrow, life });
+    //res.send({ today, tomorrow, life });
 
-  /* VALIDATION */
+    /* VALIDATION */
 
-  //reject if any field is empty
+    //reject if any field is empty
 
-  /* SENTIMENT
+    /* SENTIMENT
   ANALYSIS */
 
-  //analyze today
-
-  const resultToday = sentiment.analyze(today);
-
-  //analyze tomorrow
-
-  //analyze life
-
-  //average the scores,comparativScores
-
-  /* SAVE ANALYSIS
+    //analyze today
+    const resultToday = sentiment.analyze(today);
+    //analyze tomorrow
+    const resultTomorrow = sentiment.analyze(tomorrow);
+    //analyze life
+    const resultLife = sentiment.analyze(life);
+    //average the scores,comparativScores
+    const averageScore =
+      (resultToday.score + resultTomorrow.score + resultLife.score) / 3;
+    const averageComparativeScore =
+      (resultToday.comparative +
+        resultTomorrow.comparative +
+        resultLife.comparative) /
+      3;
+    /* SAVE ANALYSIS
   IN DB */
+    //req.user.id will contain userId if request is authorized
+    //create new sentiment instance in db with respective scores
 
-  //req.user.id will contain userId if request is authorized
+    // let result = await Sentiment.create({
+    //   score: averageScore,
+    //   comparativeScore: averageComparativeScore,
+    //   userId: request.user.id,
+    // });
 
-  //create new sentiment instance in db with respective scores
-
-  /* RESPONSE TO
+    /* RESPONSE TO
   CLIENT */
+    //send new sentiment instance to client
 
-  //send new sentiment instance to client
+    // return res.status(204).send({ ...result.dataValues });
+  } catch (error) {
+    next(error);
+  }
 });
 
 module.exports = router;
