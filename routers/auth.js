6333d557850737c7a331a0abe934d6db5039d192
nmegrant <nme.grant@gmail.com>
2020-07-01 16:15:22 +0200

    Add messages to errors for display on front end

--- routers/auth.js
+++ routers/auth.js
@@ -37,7 +37,9 @@ router.post("/login", async (req, res, next) => {
 router.post("/signup", async (req, res) => {
   const { email, password, name } = req.body;
   if (!email || !password || !name) {
-    return res.status(400).send("Please provide an email, password and a name");
+    return res
+      .status(400)
+      .send({ message: "Please provide an email, password and a name" });
   }
 
   try {
