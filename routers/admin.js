944f3fafc1d1662a26cbd9fc6b03387de51896a8
nmegrant <nme.grant@gmail.com>
2020-07-02 10:35:05 +0200

    Set up endpoint to fetch a user's data

--- routers/admin.js
+++ routers/admin.js
@@ -1,4 +1,5 @@
 const { Router } = require("express");
+const Sentiment = require("sentiment");
 const User = require("../models/").user;
 
 const router = new Router();
@@ -7,6 +8,22 @@ router.get("/users", async (req, res, next) => {
   try {
     const users = await User.findAll();
     return res.status(200).send(users);
+  } catch (error) {
+    next(error);
+  }
+});
+
+router.get("/users/:id", async (req, res, next) => {
+  try {
+    const userId = req.params.id;
+    const user = await User.findByPk(userId, {
+      include: [{ model: Sentiment }],
+    });
+    if (!user) {
+      return res.status(404).send({ message: "User and/or data not found" });
+    }
+    delete user.dataValues["password"];
+    return res.status(200).send(user);
   } catch (error) {
     next(error);
   }
